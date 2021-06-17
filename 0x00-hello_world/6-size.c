e558acfe1571fc38735cb17ff5e6aa79d5ee4c5c
root <adegokedavid161997@gmail.com>
2021-06-17 11:41:03 -0700

    Added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{printf(sizeof(char));
+printf(sizeof(int));
+printf(sizeof(long));
+printf(sizeof(long long int));
+printf(sizeof(float));
+}
