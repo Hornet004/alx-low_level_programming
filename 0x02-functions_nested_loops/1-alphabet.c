a08ae95840c9b2bd7382d59856685f547fe2cd96
root <adegokedavid161997@gmail.com>
2021-06-22 07:29:55 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,126 @@
+#include "holberton.h"
+
+/**
+*Print_alphabets in lowercase
+*/
+
+void print_alphabet(void)
+{
+    char a;
+    for (a= 'a'; a<='z'; a++)
+    {
+        _putchar(a);
+    
+    }
+    _putchar('\n');
+
+}rint_alphabets in lowercase
+*/
+
+void print_alphabet(void)
+{
+  char a;
+  for (a= 'a'; a<='z'; a++)
+    {
+      _putchar(a);
+
+    }
+  _putchar('\n');
+
+}~#include "holberton.h"
+
+/**
+*Print_alphabets in lowercase
+*/
+
+void print_alphabet(void)
+{
+    char a;
+    for (a= 'a'; a<='z'; a++)
+    {
+        _putchar(a);
+    
+    }
+    _putchar('\n');
+
+}#include "holberton.h"
+
+/**
+*Print_alphabets in lowercase
+*/
+
+void print_alphabet(void)
+{
+    char a;
+    for (a= 'a'; a<='z'; a++)
+    {
+        _putchar(a);
+    
+    }
+    _putchar('\n');
+
+}#include "holberton.h"
+
+/**
+*Print_alphabets in lowercase
+*/
+
+void print_alphabet(void)
+{
+    char a;
+    for (a= 'a'; a<='z'; a++)
+    {
+        _putchar(a);
+    
+    }
+    _putchar('\n');
+
+}#include "holberton.h"
+
+/**
+*Print_alphabets in lowercase
+*/
+
+void print_alphabet(void)
+{
+    char a;
+    for (a= 'a'; a<='z'; a++)
+    {
+        _putchar(a);
+    
+    }
+    _putchar('\n');
+
+}#include "holberton.h"
+
+/**
+*Print_alphabets in lowercase
+*/
+
+void print_alphabet(void)
+{
+    char a;
+    for (a= 'a'; a<='z'; a++)
+    {
+        _putchar(a);
+    
+    }
+    _putchar('\n');
+
+}#include "holberton.h"
+
+/**
+*Print_alphabets in lowercase
+*/
+
+void print_alphabet(void)
+{
+    char a;
+    for (a= 'a'; a<='z'; a++)
+    {
+        _putchar(a);
+    
+    }
+    _putchar('\n');
+
+}
