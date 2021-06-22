26e252c3a87f6b7549d282d0e9ea504a8168209f
root <adegokedavid161997@gmail.com>
2021-06-22 08:59:00 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar("043");
+		_putchar("+");
 		return (1);
 	} else if (n < 0)
 	{
-		_putchar("045");
+		_putchar("-");
 		return (-1);
 	}
-	_putchar("048");
+	_putchar("0");
 	return (0);
 
 }
