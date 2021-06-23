79ada286bdbb225e338fdc9505e3c2f77beb26f6
root <adegokedavid161997@gmail.com>
2021-06-23 02:23:26 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
+int add(int a, int b)
+{
+	return (a+b);
+}
