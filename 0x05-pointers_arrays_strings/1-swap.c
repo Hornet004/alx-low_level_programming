e5a4b677fa60aab1575916c60ff0c03d23d15594
root <adegokedavid161997@gmail.com>
2021-06-28 07:37:06 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps values of 2 integers
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
