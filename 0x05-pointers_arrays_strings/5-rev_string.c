96345c690f57e9a1f167a118f318deb8f05f5ceb
root <adegokedavid161997@gmail.com>
2021-06-28 09:18:28 -0700

    Added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverse a sting
+ *
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int length = 0, i, n;
+	char tmp;
+
+	while (*s != 0)
+	{
+		length++;
+		s++;
+	}
+/*reset the pointer to point to the first element*/
+	s = s - length;
+	n = length - 1;
+	for (i = 0; i < length / 2; i++)
+	{
+		tmp = *(s + i);
+		*(s + i) = *(s + n);
+		*(s + n) = tmp;
+		n--;
+	}
+
+}
