2d8f123b7c8dda00d88c74aa053b0f90342d3599
root <adegokedavid161997@gmail.com>
2021-06-28 07:30:27 -0700

    Added 0-reset_to_98.c and Readme file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- *reset_to_98 - update value the pointer point to
- *@n: pointer
- *Return: Void
-*/
+ * reset_to_98 - update value the pointer point to
+ *
+ * @n: pointer
+ *
+ * Return: Void
+ */
 
 void reset_to_98(int *n)
 {
-int m = 98;
-*n = &m;
-printf("n = %d", n);
-return (0);
+	*n = 98;
 }
