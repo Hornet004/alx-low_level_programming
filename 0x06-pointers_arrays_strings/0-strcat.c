ba6b8eb6f558dc2eee9e002f54b9bb6102b5b0d8
root <adegokedavid161997@gmail.com>
2021-06-30 04:19:35 -0700

    created the Directory 0x06-pointers_arrays_strings and and file 0-strcat.c with README and gitignore files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+*/
+char *_strcat(char *dest, char *src)
+{
+char temp[] = dest + src;
+return (temp);
+}
