02aaaa2175f5314a864905408537fc78ab41491d
root <adegokedavid161997@gmail.com>
2021-06-30 17:35:20 -0700

    Added 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,46 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ * _strlen - Returns length of a string
+ * @s: Pointer to string
+ * Return: Length of c
 */
+
+int _strlen(char *s)
+{
+	int index;
+
+	index = 0;
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	return (index);
+}
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Pointer to string to be conatenated upon
+ * @src: Pointer to string to append to dest
+ * Return: Pointer to dest
+*/
+
 char *_strcat(char *dest, char *src)
 {
-char temp[] = dest + src;
-return (temp);
+	char *destAddress;
+	int destLen;
+
+	destAddress = dest;
+	destLen = _strlen(dest);
+	destAddress = destAddress + destLen;
+	while (*src != '\0')
+	{
+		*destAddress = *src;
+		src++;
+		destAddress++;
+	}
+
+	*destAddress = '\0';
+
+	return (dest);
 }
