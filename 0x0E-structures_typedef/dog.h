3cbdcc6a0e22ba861ce9250639ffa7d8a0d7df7d
root <adegokedavid161997@gmail.com>
2021-07-19 16:59:35 -0700

    Added 1-init_dog.c and modified dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct my_struct - Short description
- * @a: First member
- * @b: Second member
- * @c: Third member
- *
- * Description: Longer description
+ * struct dog - struct for dog
+ * @name: it's name
+ * @age: it's age
+ * @owner: it's owner
+ * description: it's a struct for a dog woof woof
  */
-struct dog
+
+typedef struct dog
 {
-char *name, *owner;
-float age;
-};
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
