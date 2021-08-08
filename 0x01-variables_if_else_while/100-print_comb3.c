53c7e99e30d35008aafed735aff2be4b3961cdc1
root <adegokedavid161997@gmail.com>
2021-08-08 13:48:59 -0700

    Added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, n;
+    
+    for(i = 48; i < 58; i++)
+    {
+        for(n = 49; n < 58; n++)
+        {
+            putchar(i);
+            putchar(n);
+            if((i == 56) && (n == 57))
+            {
+                exit(0);
+            }
+            putchar(',');
+            putchar(' ');
+            
+            
+            
+        }
+        // for(j = 0; j <= 9; j++)
+        // {
+        //   k = (j += i); 
+        //   putchar(k);
+        // }
+    }
+    putchar('\n');
+    return 0;
+}
