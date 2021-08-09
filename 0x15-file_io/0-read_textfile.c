867ebcff700808f7988be4b08715737936c9b3a8
root <adegokedavid161997@gmail.com>
2021-08-09 05:40:44 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  ssize_t = fd;
+  fd = open("filename", O_RDWR);
+  if(filename == NULL)
+    return(0);
+  retun(0);
+}
